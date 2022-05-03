ac0e7278bcd7d93d775d4bf37d5bfc050f79c9a2
name <mail>
2022-05-04 00:14:38 +0300

    MASTER

---
+++ init.cmd
@@ -0,0 +1,7 @@
+::git init
+git add . "start"
+git commit -a -m "start"
+::git config --global user.name "name"
+::git config --global user.email "mail"
+git remote add origin git@github.com:alexanykey/alexanykey.github.io.git
+git push -u origin master
